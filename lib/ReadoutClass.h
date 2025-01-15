770386f6e7a40ff6b1e1c12e0245c6e867ff3afb
Gregory S. Tucker <gregory.tucker@ess.eu>
2025-01-15 13:25:15 +0100

    [Ref] Implement most of issue 1 (#2)
    A C producer can send weighted event (ray) data to the Readout object, which optionally sends UDP packets to a defined address and port (where an Event Formation Unit should be listening) and/or stores the the ray information to a specified HDF5 file.
    UDP sending (and receiving, in testing) is now achieved through `libcluon`, simplifying the Readout object. The HighFive HDF5 interface is used.
    A small C++ command line utility is provided to read and replay the stored information.
    Minimal testing is now performed on Windows, macOS, and Linux; raising the likelihood that Readout will work on multiple platforms.
    
    Some things are missing or could be improved still:
    1. versioning of the produced library file should follow the Git tag for this repository. A mechanism exists to use the tag, but as there has not been a tag yet the mechanism does not yet work.
    2. Since the Readout object now knows the weight of each ray, it can (and should) make the ray-to-event determination instead of replicating this code in each component definition file. This will also allow the replayed rays to be converted to events. An enumerated value (or integer flag) could be used to select the conversion mode `{0: weight>0, 1: random_Poisson(weight), ...}`.

---
+++ lib/ReadoutClass.h
@@ -0,0 +1,189 @@
+// Copyright (C) 2022 European Spallation Source, ERIC. See LICENSE file
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief UDP readout generator class
+///
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "cluon-complete.hpp"
+
+#include <string>
+#include <utility>
+#include <optional>
+
+#include "Structs.h"
+#include "Readout.h"
+#include "enums.h"
+#include "hdf_interface.h"
+#include "version.hpp"
+#include "efu_time.h"
+
+class Readout {
+public:
+  Readout(
+      std::string IpAddress,
+        const int UDPPort,
+        const int TCPPort,
+        const int Type=0x34,
+        efu_time p = efu_time(1),
+        efu_time t = efu_time()
+  ): Type(detectorType_from_int(Type)),
+     ipaddr(std::move(IpAddress)),
+     port(UDPPort),
+     tcp_port(TCPPort),
+     period(p),
+     time(t),
+     sender{ipaddr, static_cast<uint16_t>(UDPPort)}
+  {
+//    sockOpen(ipaddr, port);
+    hp = (PacketHeaderV0*)&buffer[0];
+    auto prev = time - period;
+    setPulseTime(time.high(), time.low(), prev.high(), prev.low());
+    newPacket();
+  }
+
+  ~Readout() {
+    // ensure any buffered data is sent before the object is destroyed
+    send();
+  }
+
+  // Adds a readout to the transmission buffer.
+  // If there is no room left, transmit and initialize a new packet
+  void addReadout(uint8_t Ring, uint8_t FEN, double tof, double weight, const void * data);
+  // Specializations for handled data types
+  void addReadout(uint8_t Ring, uint8_t FEN, efu_time t, const CAEN_readout_t * data);
+  void addReadout(uint8_t Ring, uint8_t FEN, efu_time t, const TTLMonitor_readout_t * data);
+  void addReadout(uint8_t Ring, uint8_t FEN, efu_time t, const DREAM_readout_t * data);
+  void addReadout(uint8_t Ring, uint8_t FEN, efu_time t, const VMM3_readout_t * data);
+
+  void saveReadout(uint8_t Ring, uint8_t FEN, double tof, double weight, const void * data);
+  template<class T> void saveReadout(T data){
+    if (file.has_value() and dataset.has_value()) {
+      auto & ds = dataset.value();
+      // the dataset should be 1-D ... hopefully that's true
+      auto pos = ds.getDimensions().back();
+      auto size = pos + 1;
+      ds.resize({size});
+      ds.select({pos}, {1}).write(data); // select(offset, count)
+    }
+  }
+
+  // send the current data buffer
+  int send();
+
+  // Update the pulse and previous pulse times
+  void setPulseTime(uint32_t PHI, uint32_t PLO, uint32_t PPHI, uint32_t PPLO);
+
+  void update_time(){
+    auto now = efu_time();
+    if ((now - time) >= &period){
+      now = time + period * ((now - time) / period);
+    }
+    // The ESS Caen EFUs require (now - prev) <= 5 * rep; so we should fake it
+    if ((now - time) > (period * 5u)) {
+      time = now - period;
+    }
+    send();
+    setPulseTime(now.high(), now.low(), time.high(), time.low());
+    newPacket();
+    time = now;
+  }
+
+  // Query the current pulse and previous pulse times
+  [[nodiscard]] std::pair<uint32_t, uint32_t> lastPulseTime() const;
+  [[nodiscard]] std::pair<uint32_t, uint32_t> prevPulseTime() const;
+  [[nodiscard]] std::pair<uint32_t, uint32_t> lastEventTime() const;
+
+  // Initialize a new packet with no readouts
+  void newPacket();
+
+  // Tell the (remote) device to shut down
+  int command_shutdown() const;
+
+  // Set verbosity via enum
+  int verbose(const Verbosity v){
+    switch (v) {
+      case Verbosity::details: verbosity=3; break;
+      case Verbosity::info: verbosity=2; break;
+      case Verbosity::warnings: verbosity=1; break;
+      case Verbosity::errors: verbosity=0; break;
+      case Verbosity::silent: verbosity=-1; break;
+      default: verbosity=0;
+    }
+    return verbosity;
+  }
+  int verbose(const int v){verbosity = v; return verbosity;}
+
+  void dump_to(const std::string & filename, const std::string & dataset_name = "events"){
+    // Do we need to keep this reference ot the file?
+    file = HighFive::File(filename, HighFive::File::OpenOrCreate);
+    // we want to output an events list which can grow forever
+    auto dataspace = HighFive::DataSpace({0}, {HighFive::DataSpace::UNLIMITED});
+    // but should chunk file operations to avoid too much disk IO?
+    HighFive::DataSetCreateProps props;
+    props.add(HighFive::Chunking(std::vector<hsize_t>{100}));
+    auto dt = datatype();
+    dt.commit(file.value(), readoutType_name(readoutType_from_detectorType(Type)));
+    dataset = file.value().createDataSet(dataset_name, dataspace, dt, props);
+    // Assign useful information as attributes:
+    /* FIXME C++20 has char8_t but C++17 does not, so these strings _might_ already be chars
+     *       instead of unsigned chars. If that's the case this lambda is a non-op.
+     */
+    auto u8str = [](const auto * p){return std::string(reinterpret_cast<const char *>(p));};
+    file->createAttribute<std::string>("program", "libreadout");
+    file->createAttribute<std::string>("version", u8str(libreadout::version::version_number));
+    file->createAttribute<std::string>("revision", u8str(libreadout::version::git_revision));
+    file->createAttribute<std::string>("events", dataset_name);
+    dataset->createAttribute("detector", detectorType_name(Type));
+    dataset->createAttribute("readout", readoutType_name(readoutType_from_detectorType(Type)));
+  }
+
+  void enable_network() {network = true;}
+  void disable_network() {network = false;}
+
+private:
+  HighFive::CompoundType datatype() const {
+    using namespace HighFive;
+    switch (readoutType_from_detectorType(Type)){
+      case ReadoutType::CAEN: return create_datatype<CAEN_event>();
+      case ReadoutType::TTLMonitor: return create_datatype<TTLMonitor_event>();
+      case ReadoutType::DREAM: return create_datatype<DREAM_event>();
+      case ReadoutType::VMM3: return create_datatype<VMM3_event>();
+      default: throw std::runtime_error("Saving this readout type is not implemented yet!");
+    }
+  }
+
+  void check_size_and_send();
+
+  // Packet header
+  uint32_t phi{0}; // pulse and prev pulse high and low
+  uint32_t plo{0};
+  uint32_t pphi{0};
+  uint32_t pplo{0};
+
+  uint32_t lasthi{0};
+  uint32_t lastlo{0};
+
+  int SeqNum{0};
+  int OutputQueue{0};
+  DetectorType Type;
+
+  // TX Buffer
+  PacketHeaderV0 *hp{};
+  char buffer[9000]{};
+  const int MaxDataSize{8950};
+  int DataSize{0};
+  // IP and port number
+  std::string ipaddr;
+  int port{9000};
+  int tcp_port{8888};
+  int verbosity{0};
+
+  std::optional<HighFive::File> file{std::nullopt};
+  std::optional<HighFive::DataSet> dataset{std::nullopt};
+  bool network{true};
+  efu_time period, time;
+  cluon::UDPSender sender;
+};
---
+++ test/replay_test.cpp
@@ -0,0 +1,153 @@
+#define CATCH_CONFIG_MAIN
+#include <filesystem>
+#include <catch2/catch_test_macros.hpp>
+#include "cluon-complete.hpp"
+
+#include <Readout.h>
+#include <Structs.h>
+#include <reader.h>
+#include <replay.h>
+#include "test_utils.h"
+
+#if defined(_MSC_VER) || defined(__MINGW32__)
+#include <process.h>
+int process_id() {return _getpid();}
+#else
+#include <unistd.h>
+int process_id() {return static_cast<int>(getpid());}
+#endif
+
+std::string pid_filename(const std::string& base, const std::string& ext){
+  return base + std::to_string(process_id()) + ext;
+}
+
+TEST_CASE("Store and retrieve CAEN packets", "[c][CAEN][io]"){
+  // create a temporary filename where we can store an HDF5 file:
+  namespace fs=std::filesystem;
+  auto tdir = fs::temp_directory_path();
+  fs::path filepath = tdir;
+  filepath /= fs::path(pid_filename("replay_caen",".h5"));
+  auto filename = filepath.string();
+
+  const uint16_t max{1000};
+  uint32_t detector_type{0x34};
+
+  auto detector_efu = readout_create("127.0.0.1", 9003, 8888, 1 / 14., static_cast<int>(detector_type));
+  readout_disable_network(detector_efu); // avoid sending packets -- just in case
+  readout_dump_to(detector_efu, filename.data());
+  // add events, one-by-one, to the HDF5 file
+  readout_newPacket(detector_efu);
+  CAEN_readout_t caen_data;
+  for (uint16_t i=0; i<max; ++i){
+    uint8_t ring = 1;
+    uint8_t fen = 0;
+    uint8_t tube = 3;
+    double tof = static_cast<double>(i)/static_cast<double>(max);
+    caen_data.channel = tube;
+    caen_data.a = i;
+    caen_data.b = max - i;
+    caen_data.c = 0;
+    caen_data.d = 0;
+    readout_add(detector_efu, ring, fen, tof, static_cast<double>(i), static_cast<const void *>(&caen_data));
+  }
+  readout_send(detector_efu);
+  readout_destroy(detector_efu);
+
+  // with the ReadoutClass object destroyed, we should be able to re-open the file and check its contents
+  auto reader = Reader(filename);
+  REQUIRE(ReadoutType::CAEN == reader.readout_type());
+  REQUIRE(max == reader.size());
+  for (size_t i=0; i<reader.size(); ++i){
+    auto event = reader.get_CAEN(i, 1).front();
+    REQUIRE(1 == event.ring);
+    REQUIRE(0 == event.fen);
+    REQUIRE(3 == event.channel);
+    double tof = static_cast<double>(i)/static_cast<double>(reader.size());
+    REQUIRE(tof == event.time);
+    REQUIRE(static_cast<double>(i) == event.weight);
+    REQUIRE(i == event.a);
+    REQUIRE(max - static_cast<uint16_t>(i) == event.b);
+    REQUIRE(0 == event.c);
+    REQUIRE(0 == event.d);
+  }
+}
+
+// Test filling a file, then reading back its contents and sending them to a UDP listener ...
+//
+TEST_CASE("Store, replay and receive TTLMonitor packets","[c][TTLMonitor][io]"){
+  // create a temporary filename where we can store an HDF5 file:
+  namespace fs=std::filesystem;
+  auto tdir = fs::temp_directory_path();
+  fs::path filepath = tdir;
+  filepath /= fs::path(pid_filename("replay_ttlmonitor",".h5"));
+  auto filename = filepath.string();
+
+  const uint16_t max{1000};
+  uint32_t monitor_type{0x10};
+  int monitor_port{9004};
+
+  auto stats = std::make_shared<UDPStats>();
+
+  cluon::UDPReceiver monitor_receiver("127.0.0.1", monitor_port,
+    [stats,monitor_type](std::string && data, std::string &&, std::chrono::system_clock::time_point &&) noexcept {
+      // data must contain [PacketHeaderV0, readout, readout, ...].
+      auto ptr = data.data();
+      auto * header = reinterpret_cast<PacketHeaderV0*>(ptr);
+      REQUIRE(header->Padding0 == 0);
+      REQUIRE(header->Version == 0);
+      auto type = (header->CookieAndType) >> 24;
+      auto cookie =  (header->CookieAndType - (type << 24));
+      REQUIRE(cookie == 0x535345);  // ESS identifier
+      REQUIRE(monitor_type == type);
+      ptr += sizeof(PacketHeaderV0);
+      size_t readout_size = sizeof(struct TTLMonitorData);
+      auto readouts = (header->TotalLength - sizeof(PacketHeaderV0)) / readout_size;
+      for (size_t i=0; i < readouts; ++i){
+        auto *r = reinterpret_cast<TTLMonitorData *>(ptr + i * readout_size);
+        REQUIRE(r->Ring == 0);
+        REQUIRE(r->FEN == 100);
+        REQUIRE(r->Pos == 3);
+        REQUIRE((r->Channel == 1 || r->Channel == 0));
+      }
+      stats->packets++;
+      stats->readouts += readouts;
+    });
+  REQUIRE(monitor_receiver.isRunning());
+
+  char addr[] = "127.0.0.1";
+  auto monitor_efu = readout_create(addr, monitor_port, 8889, 1 / 14., static_cast<int>(monitor_type));
+  readout_disable_network(monitor_efu);
+  readout_dump_to(monitor_efu, filename.data());
+  readout_newPacket(monitor_efu);
+  TTLMonitor_readout_t ttl_data;
+  for (uint16_t i=0; i<max; ++i){
+    uint8_t tube = 3;
+    double tof = static_cast<double>(i)/static_cast<double>(max);
+    ttl_data.pos = tube;
+    ttl_data.channel = 0;
+    ttl_data.adc = i;
+    readout_add(monitor_efu, 0, 100, tof, 1.0, static_cast<const void *>(&ttl_data));
+    ttl_data.channel = 1;
+    ttl_data.adc = max - i;
+    readout_add(monitor_efu, 0, 100, tof, 2.0, static_cast<const void *>(&ttl_data));
+  }
+  readout_send(monitor_efu);
+  readout_destroy(monitor_efu);
+
+  REQUIRE(stats->readouts == 0); // No UDP communication has occurred yet
+  {
+    auto reader = Reader(filename);
+    REQUIRE(ReadoutType::TTLMonitor == reader.readout_type());
+    REQUIRE(2 * max == reader.size());
+  }
+  // send all events in order
+  replay_all(filename, addr, monitor_port, Replay::ALL & Replay::SEQUENTIAL);
+  // the lambda function defined above gets called for each packet
+  // and all 2 * max produced events are received
+  auto expected = 2 * max;
+  if (stats->readouts < expected){
+    // wait a bit in case the receiver is doing something?
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+  REQUIRE(stats->readouts == expected);
+}
