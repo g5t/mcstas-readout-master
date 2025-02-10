61adcad7bcce4d7dca8275a39273a08d06aa3559
Gregory Tucker <gregory.tucker@ess.eu>
2025-02-10 09:04:21 +0100

    [Fix] binary path on macOS, [Add] CTest entries for binaries

--- src/config/helper.cpp
+++ src/config/helper.cpp
@@ -8,11 +8,33 @@ std::filesystem::path executable_path() {
   });
   return std::filesystem::path(exe_path);
 }
+
+#elif defined(__APPLE__)
+
+#include <mach-o/dyld.h>
+
+std::filesystem::path executable_path() {
+  char buffer[1024];
+  uint32_t size = sizeof(buffer);
+  // If the buffer is not large enough, size is set to the correct size and return value is -1
+  if (_NSGetExecutablePath(buffer, &size) == 0) {
+    return std::filesystem::canonical(buffer);
+  } else {
+    std::vector<char> largerBuffer(size);
+    if (_NSGetExecutablePath(largerBuffer.data(), &size) == 0) {
+      return std::filesystem::canonical(largerBuffer.data());
+    }
+  }
+  throw std::runtime_error("Unable to determine executable path");
+}
+
 #else
+
 std::filesystem::path executable_path(){
   std::filesystem::path result("/proc/self/exe");
   return std::filesystem::canonical(result);
 }
+
 #endif
 
 
